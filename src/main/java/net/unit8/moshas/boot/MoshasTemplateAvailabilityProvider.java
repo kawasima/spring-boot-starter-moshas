b3dcc1ad8b4290947725fc49900ba4a74c9fbdb7
k.hasegawa <k.hasegawa@future-shop.co.jp>
2016-03-09 14:02:45 +0900

    Determining whether the Moshasengine is included in classLoader

--- src/main/java/net/unit8/moshas/boot/MoshasTemplateAvailabilityProvider.java
+++ src/main/java/net/unit8/moshas/boot/MoshasTemplateAvailabilityProvider.java
@@ -3,6 +3,7 @@ package net.unit8.moshas.boot;
 import org.springframework.boot.autoconfigure.template.TemplateAvailabilityProvider;
 import org.springframework.core.env.Environment;
 import org.springframework.core.io.ResourceLoader;
+import org.springframework.util.ClassUtils;
 
 /**
  * @author kawasima
@@ -10,9 +11,15 @@ import org.springframework.core.io.ResourceLoader;
 public class MoshasTemplateAvailabilityProvider implements TemplateAvailabilityProvider {
     @Override
     public boolean isTemplateAvailable(String view, Environment environment, ClassLoader classLoader, ResourceLoader resourceLoader) {
-        String prefix = environment.getProperty("spring.moshas.prefix", "/META-INF/templates");
-        String suffix = environment.getProperty("spring.moshas.suffix", ".html");
 
-        return resourceLoader.getResource(prefix + view + suffix).exists();
+        if (ClassUtils.isPresent("net.unit8.moshas.MoshasEngine", classLoader)) {
+
+            String prefix = environment.getProperty("spring.moshas.prefix", "/META-INF/templates");
+            String suffix = environment.getProperty("spring.moshas.suffix", ".html");
+
+            return resourceLoader.getResource(prefix + view + suffix).exists();
+        }
+
+        return false;
     }
 }
